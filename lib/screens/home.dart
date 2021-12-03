ca267dd695f7fae364a56ad2d318bc3f9dce56fc
onderkoca <ytukom@gmail.com>
2021-12-03 12:17:29 +0300

    salih yazıldı

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("")
-    );
+    return Container(child: Text("Salih"));
   }
 }
